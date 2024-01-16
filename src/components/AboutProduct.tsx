c9d6a50411aba955656ccd1ff4def8f8783d75b9
Benni-beep <krikelkr4kel@gmail.com>
2024-01-16 08:36:11 +0100

    feat: add AboutProduct layout & styling

--- src/components/AboutProduct.tsx
+++ src/components/AboutProduct.tsx
@@ -1,15 +1,20 @@
+import "./AboutProduct.css";
 export const AboutProduct = () => {
   return (
-    <>
-      <h2>About our furniture</h2>
-      <p>
-        Our multifunctional collection blends design and function to suit your
-        individual taste. Make each room unique, or pick a cohesive theme that
-        best express your interests and what inspires you. Find the furniture
-        pieces you need, from traditional to contemporary styles or anything in
-        between. Product specialists are available to help you create your dream
-        space.
-      </p>
-    </>
+    <div className="about-product-wrapper">
+      <img src="images/image-about-dark.jpg" alt="" />
+      <div className="text-wrapper">
+        <h2>ABOUT OUR FURNITURE</h2>
+        <p>
+          Our multifunctional collection blends design and function to suit your
+          individual taste. Make each room unique, or pick a cohesive theme that
+          best express your interests and what inspires you. Find the furniture
+          pieces you need, from traditional to contemporary styles or anything
+          in between. Product specialists are available to help you create your
+          dream space.
+        </p>
+      </div>
+      <img src="images/image-about-light.jpg" alt="" />
+    </div>
   );
 };
